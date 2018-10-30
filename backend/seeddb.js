2d1c3ace26d534970f86c0fb21894c6ed3d2819b
hello-klol <k.a.clark87@gmail.com>
2018-10-30 14:31:13 +0000

    Change seed script to use async

--- backend/seeddb.js
+++ backend/seeddb.js
@@ -1,75 +1,86 @@
 #! /usr/bin/env node
 
-    // export MONGO_URL="mongodb://admin:l0lc4t5@localhost:27017/campaigns"
-// to clear and start over
+// export MONGO_URL="mongodb://admin:l0lc4t5@localhost:27017/campaigns"
+
+// to clear and start over"
 // use campaigns;
 // db.dropDatabase();
 
+const models = require('./models')
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+const data = require('./data.json')
 
-const models = require('./models');
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-const data = require('./data.json');
-
-
-const mongoDbURL = process.env.MONGO_URL;
-console.log('Lets begin');
+const mongoDbURL = process.env.MONGO_URL
 
 // for case where KeyWords / interests have different names but same meaning
-function cleanTargetAudience(aud) {
-    return  Object.assign(aud, {
-        'tags': aud.interests || aud.KeyWords || ''
-    })
+function cleanTargetAudience (aud) {
+  return Object.assign(aud, {
+    'tags': aud.interests || aud.KeyWords || ''
+  })
 }
 
 // for case where data is called header_1, header_2
-function cleanCreatives(cre) {
-    return Object.assign(cre, {
-        'header': cre.header || cre.header_1 || '',
-        'subheader': cre.header_2 || ''
-    });
+function cleanCreatives (cre) {
+  return Object.assign(cre, {
+    'header': cre.header || cre.header_1 || '',
+    'subheader': cre.header_2 || ''
+  })
 }
 
-mongoose.createConnection(mongoDbURL)
-    .then((connection) => {
-        const { Campaign, Platform, TargetAudience, Creatives, Insights } = models.create(connection);
+async function seedDatabase (url) {
+  const conn = await mongoose.createConnection(url)
+  const { Campaign, Platform, TargetAudience, Insights, Creatives } = models.create(conn)
 
-        return Promise.all(data.map((campaign) => {
+  function saveTargetAudience (data) {
+    const targetAudienceData = cleanTargetAudience(data)
+    return new TargetAudience(targetAudienceData).save()
+  }
 
-            console.log('Get platforms')
-            return Promise.all(Object.keys(campaign.platforms).map((platformKey) => {
+  function saveCreatives (data) {
+    const creativesData = cleanCreatives(data)
+    return new Creatives(creativesData).save()
+  }
 
-                const platform = campaign.platforms[platformKey];
-                const targetAudience = new TargetAudience(cleanTargetAudience(platform.target_audiance));
-                const creatives = new Creatives(cleanCreatives(platform.creatives));
-                const insights = new Insights(platform.insights);
+  function saveInsights (data) {
+    new Insights(data).save()
+  }
 
-                console.log('Saving target_audience, creatives, insights')
-                // platform relies on these others to be persisted first
-                return Promise.all([targetAudience.save(), creatives.save(), insights.save()])
+  function savePlatforms (campaign) {
+    return Object.keys(campaign.platforms).map(async (platformKey) => {
+      const platformData = campaign.platforms[platformKey]
 
-                    .then(([targetAudience, creatives, insights])=>{
-                        console.log('Saving platform')
-                        return new Platform(Object.assign(platform, {
-                            targetAudience, creatives, insights
-                        })).save();
+      const [targetAudience, creatives, insights] = await Promise.all([
+        saveTargetAudience(platformData.target_audiance),
+        saveCreatives(platformData.creatives),
+        saveInsights(platformData.insights)
+      ])
 
-                    });
+      return new Platform(Object.assign(platformData, {
+        target_audience: targetAudience, creatives, insights
+      })).save()
+    })
+  }
 
-            })).then((platforms) => {
-                console.log('Platform saved')
-                console.log('Save campaign')
-                return new Campaign(Object.assign(campaign, {
-                    platforms
-                })).save();
-            });
+  await Promise.all(
+    data.map(async (campaign) => {
+      const platforms = await Promise.all(savePlatforms(campaign))
 
-        })).then(() => {
-            console.log('Upload complete!');
-        }).catch((err) => {
-            console.log(err);
-        }).finally(() => {
-            connection.close();
-        });
+      return new Campaign(Object.assign(campaign, {
+        platforms
+      })).save()
+    })
+  )
 
-})
+  conn.close()
+}
+
+async function main (url) {
+  try {
+    await seedDatabase(url)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+main(mongoDbURL)
